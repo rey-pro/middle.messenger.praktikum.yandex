6a51a00ab0aeab8bbf1e70cf6263c0e48914cc23
rey <KeyBLJkee88@yandex.ru>
2022-03-21 19:14:45 +0500

    v0.3

---
+++ src/pages/profile/profile.tml.js
@@ -0,0 +1,50 @@
+export default `
+<main class="main">
+    <div class="profile-navigation" ><a href="/chat.html">
+        <button class="button profile-navigation__button">←</button>
+    </a></div>
+    <div class="profile-main" >
+        <div class="profile-info" >
+            <div class="profile-info__image" >
+                <div class="profile-info__image-edit"><p class="profile-info__image-edit-text">Поменять аватар</p></div>
+            </div>
+            <h4 class="profile-info__name">Иван </h4>
+            <div class="profile-info__fields-container" >
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Почта</p>
+                    <p class="profile-info__field-value">test@mail.ru</p></div>
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Логин</p>
+                    <p class="profile-info__field-value">test</p></div>
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Имя</p>
+                    <p class="profile-info__field-value">Иван</p></div>
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Фамилия</p>
+                    <p class="profile-info__field-value">Иванов</p></div>
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Имя в чате</p>
+                    <p class="profile-info__field-value">Ivan</p></div>
+                <div class="profile-info__field" ><p
+                        class="profile-info__field-name">Телефон</p>
+                    <p class="profile-info__field-value">+79999999999</p></div>
+            </div>
+            <div class="profile-info__fields-container" >
+                <div class="profile-info__field" >
+                    <button class="button profile-info__edit-button">Изменить данные
+                    </button>
+                </div>
+                <div class="profile-info__field" >
+                    <button class="button profile-info__edit-button">Изменить пароль
+                    </button>
+                </div>
+                <div class="profile-info__field" >
+                    <button class="button profile-info__logout-button" >
+                        Выйти
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+`
