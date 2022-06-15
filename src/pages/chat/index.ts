9a88ced2abe2470100ea26c38e7f6a0882c82aff
rey <KeyBLJkee88@yandex.ru>
2022-06-15 16:52:12 +0500

    sprint 2: chat

---
+++ src/pages/chat/index.ts
@@ -0,0 +1,16 @@
+import { renderDOM, registerComponent }  from '../../core';
+import {Chat} from "./chat";
+
+import Button from "../../components/button";
+import Input from "../../components/input";
+import InputField from "../../components/input-field";
+import InputError from "../../components/input-error";
+
+registerComponent(Button, 'Button');
+registerComponent(Input, 'Input');
+registerComponent(InputField, 'InputField');
+registerComponent(InputError, 'InputError');
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderDOM(new Chat([]));
+});
