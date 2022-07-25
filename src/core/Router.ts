4c0fcf68791fbdd06f4d7ec0f47ed6285240c65d
rey <KeyBLJkee88@yandex.ru>
2022-07-25 22:25:42 +0500

    split Route and Router

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,43 +1,4 @@
-import renderPage from "./renderPage";
-import Block from "./Block";
-
-class Route {
-    protected _pathname;
-    protected _blockClass;
-    protected _block: Block | null;
-    protected _props;
-
-    constructor(pathname: string, view: any, props: object) {
-        this._pathname = pathname;
-        this._blockClass = view;
-        this._block = null;
-        this._props = props;
-    }
-
-    navigate(pathname: string) {
-        if (this.match(pathname)) {
-            this.render();
-        }
-    }
-
-    leave() {
-        if (this._block) {
-            this._block.hide();
-        }
-    }
-
-    match(pathname: string) {
-        return pathname === this._pathname;
-    }
-
-    render() {
-        if (!this._block) {
-            this._block = new this._blockClass(this._props);
-        }
-    
-        renderPage(this._block!);
-    }
-}
+import Route from "./Route";
 
 export class Router {
     private static __instance: Router;
